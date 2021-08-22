445d7a8c838e3a24fcddef69dab1dcf944aeedc4
sharukhsm <sharukhsm@gmail.com>
2021-08-22 17:28:34 +0530

    add crud components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-//Semanti ui
-import "semantic-ui-css/semantic.min.css";
 
 function App() {
   return (
     <div className="main">
-      <h2 className="main-header"> React Crudd Operations</h2>
+      <h2 className="main-header"> React Crud Operations</h2>
     </div>
   );
 }
