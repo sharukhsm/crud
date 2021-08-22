b39c411f6b90d3539f120230540d91fee3694ea7
sharukhsm <sharukhsm@gmail.com>
2021-08-22 23:01:20 +0530

    axios setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Create from "./components/create";
 
 function App() {
   return (
     <div className="main">
       <h2 className="main-header"> React Crud Operations</h2>
+      <Create />
     </div>
   );
 }
---
+++ src/components/create.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import axios from "axios";
+
+function Create() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
+
+  const postData = () => {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(checkbox);
+  };
+
+  return (
+    <div>
+      <Form className="create-form">
+        <Form.Field>
+          <label>First Name</label>
+          {/* This is a controlled component.  */}
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Last Name</label>
+          <input
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <Checkbox
+            label="I agree to the Terms and Conditions"
+            onChange={(e) => setCheckbox(!checkbox)}
+          />
+        </Form.Field>
+
+        <Button onClick={postData} type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Create;
