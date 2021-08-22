72dd87ce357bde1acee954692e100fe535fb348b
sharukhsm <sharukhsm@gmail.com>
2021-08-22 16:23:43 +0530

    rename branch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "semantic-ui-css/semantic.min.css";
 function App() {
   return (
     <div className="main">
-      <h2 className="main-header"> React Crud Operations</h2>
+      <h2 className="main-header"> React Crudd Operations</h2>
     </div>
   );
 }
