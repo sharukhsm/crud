4e8106956a7c5970a0e9d1e1a7af933f801d6d83
sharukhsm <sharukhsm@gmail.com>
2021-08-23 15:45:45 +0530

    complete update component

--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "semantic-ui-react";
+import { Table, Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Read() {
   const [APIData, setAPIData] = useState([]);
@@ -12,7 +14,14 @@ function Read() {
       });
   }, []);
 
-  console.log(APIData);
+  //Setting data to local storage
+  const setData = (data) => {
+    let { id, firstName, lastName, checkbox } = data;
+    localStorage.setItem("ID", id);
+    localStorage.setItem("First Name", firstName);
+    localStorage.setItem("Last Name", lastName);
+    localStorage.setItem("Checkbox Value", checkbox);
+  };
 
   return (
     <div>
@@ -22,6 +31,7 @@ function Read() {
             <Table.HeaderCell>First Name</Table.HeaderCell>
             <Table.HeaderCell>Last Name</Table.HeaderCell>
             <Table.HeaderCell>Checked</Table.HeaderCell>
+            <Table.HeaderCell>Update</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
@@ -34,6 +44,12 @@ function Read() {
                 <Table.Cell>
                   {data.checkbox ? "Checked" : "unchecked"}
                 </Table.Cell>
+
+                <Link to="/update">
+                  <Table.Cell>
+                    <Button onClick={() => setData(data)}>Update</Button>
+                  </Table.Cell>
+                </Link>
               </Table.Row>
             );
           })}
